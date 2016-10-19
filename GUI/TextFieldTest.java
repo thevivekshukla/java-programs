bd67837745267114391eb16b2c080bd9a929ef4a
Vivek Shukla <viv3kshukla@gmail.com>
2016-10-19 23:45:47 +0530

    added gui program

---
+++ GUI/TextFieldTest.java
@@ -0,0 +1,69 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+class TextFieldFrame extends JFrame
+{
+	private JTextField textField1;
+	private JTextField textField2;
+	private JTextField textField3;
+	private JPasswordField passwordField;
+
+	public TextFieldFrame()
+	{
+		super("Testing JTextField and JPasswordField");
+		setLayout(new FlowLayout());
+
+		textField1 = new JTextField(10);
+		add(textField1);
+
+		textField2 = new JTextField("Enter text here");
+		add(textField2);
+
+		textField3 = new JTextField("Uneditable text field", 21);
+		textField3.setEditable(false);
+		add(textField3);
+
+		passwordField = new JPasswordField("Hidden text");
+		add(passwordField);
+
+		TextFieldHandler handler = new TextFieldHandler();
+		textField1.addActionListener(handler);
+		textField2.addActionListener(handler);
+		textField3.addActionListener(handler);
+	}
+
+	private class TextFieldHandler implements ActionListener
+	{
+		public void actionPerformed(ActionEvent event)
+		{
+			String string = "";
+
+			if (event.getSource() == textField1)
+				string = String.format("textField1: %s", event.getActionCommand());
+			else if (event.getSource() == textField2)
+				string = String.format("textField2: %s", event.getActionCommand());
+			else if (event.getSource() == textField3)
+				string = String.format("textField3: %s", event.getActionCommand());
+			else if (event.getSource() == passwordField)
+				string = String.format("passwordField: %s", event.getActionCommand());
+
+			JOptionPane.showMessageDialog(null, string);
+		}
+	}
+
+}
+
+
+
+
+public class TextFieldTest
+{
+	public static void main(String[] args)
+	{
+		TextFieldFrame textFieldFrame = new TextFieldFrame();
+		textFieldFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		textFieldFrame.setSize(350, 100);
+		textFieldFrame.setVisible(true);
+	}
+}
